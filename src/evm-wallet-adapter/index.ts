d35ab6b6a22989da431f559821545f5e33bdaba2
Maxwell Fortney <maxwell@paella.dev>
2022-09-29 21:07:46 -0400

    Adds network map util

--- src/evm-wallet-adapter/index.ts
+++ src/evm-wallet-adapter/index.ts
@@ -1,1 +1,16 @@
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { CrossmintEnvironment } from "../types";
+
 export * from "./adapter";
+
+export function networkToCrossmintEnvironment(network: WalletAdapterNetwork): CrossmintEnvironment {
+    switch (network) {
+        case WalletAdapterNetwork.Testnet:
+            console.error("Crossmint does not support solana testnet. Falling back to staging.");
+        case WalletAdapterNetwork.Devnet:
+            return CrossmintEnvironment.STAGING;
+        case WalletAdapterNetwork.Mainnet:
+        default:
+            return CrossmintEnvironment.PROD;
+    }
+}
